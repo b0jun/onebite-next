b96507f3853e89c4e9478844feedad72e839e121
b0jun <bjkim0228@naver.com>
2024-11-07 16:07:17 +0900

    lecture: App Router UI

---
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,20 @@
+import books from '@/mock/books.json';
+import BookItem from '@/components/book-item';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    q?: string;
+  }>;
+}) {
+  const { q } = await searchParams;
+  console.log(q);
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
