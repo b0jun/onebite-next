dd874d62ef0a14de2dcfd442c97c9fd5f84200ac
b0jun <bjkim0228@naver.com>
2024-11-25 20:40:22 +0900

    lecture: 고급라우팅 패턴
    - 병렬 라우트
    - 인터셉터 라우트
    - 모달 구현

---
+++ section09/src/actions/create-review.action.ts
@@ -0,0 +1,50 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+
+export async function createReviewAction(_: any, formData: FormData) {
+  const bookId = formData.get('bookId')?.toString();
+  const content = formData.get('content')?.toString();
+  const author = formData.get('author')?.toString();
+
+  if (!bookId || !content || !author) {
+    return {
+      status: false,
+      error: '리뷰 내용과 작성자를 입력해주세요',
+    };
+  }
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`, {
+      method: 'POST',
+      body: JSON.stringify({ bookId, content, author }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // 1. 특정 주소의 해당하는 페이지만 재검증
+    // revalidatePath(`/book/${bookId}`);
+
+    // // 2. 특정 경로의 모든 동적 페이지를 재검증 (폴더 및 파일의 경로)
+    // revalidatePath('/book/[id]', 'page');
+
+    // // 3. 특정 레아아웃을 갖는 모든 페이지를 재검증
+    // revalidatePath('/(with-searchbar)', 'layout');
+
+    // // 4. 모든 데이터 재검증
+    // revalidatePath('/', 'layout');
+
+    // 5. 태그 기준, 데이터 캐시 재검증
+    revalidatePath(`review-${bookId}`);
+    return {
+      status: true,
+      error: '',
+    };
+  } catch (error) {
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${error}`,
+    };
+  }
+}
---
+++ section09/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,56 @@
+import BookItem from '@/components/book-item';
+import BookListSkeleton from '@/components/skeleton/book-list-skeleton';
+import { BookData } from '@/types';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+async function SearchResult({ q }: { q: string }) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`, {
+    cache: 'force-cache',
+  });
+  const books: BookData[] = await response.json();
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    q?: string;
+  }>;
+}) {
+  // 현재 페이지 메타 데이터를 동적으로 생성하는 역할
+  const { q } = await searchParams;
+
+  return {
+    title: `${q} : 한입북스 검색`,
+    description: `${q}의 검색 결과입니다`,
+    openGraph: {
+      title: `${q} : 한입북스 검색`,
+      description: `${q}의 검색 결과입니다`,
+      images: ['/thumbnail.png'],
+    },
+  };
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    q?: string;
+  }>;
+}) {
+  const { q } = await searchParams;
+  return (
+    // Suspense key 값이 변경될때마다 로딩
+    <Suspense key={q || ''} fallback={<BookListSkeleton count={3} />}>
+      <SearchResult q={q || ''} />
+    </Suspense>
+  );
+}
